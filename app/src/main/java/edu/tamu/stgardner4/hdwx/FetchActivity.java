311b11cacc6a3f0ab021c9e17618731e343fefa0
Sam Gardner <stgardner4@tamu.edu>
2021-12-31 22:45:00 -0600

    add fetch activity

---
+++ app/src/main/java/edu/tamu/stgardner4/hdwx/FetchActivity.java
@@ -0,0 +1,14 @@
+package edu.tamu.stgardner4.hdwx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FetchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fetch);
+    }
+}
